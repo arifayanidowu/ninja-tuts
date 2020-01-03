0706a157fd08456435f42a24bb05c99d55663a65
Arifayan Idowu Olatubosun <arifayanidowu2@yahoo.com>
2020-01-03 10:31:56 +0100

    Modified server.js

--- server.js
+++ server.js
@@ -33,7 +33,8 @@ app.use(
 );
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "build")));
+  app.use(express.static("build"));
+
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "build", "index.html"));
   });
